269525ca656d61182eb51c47e766a74cf7205583
Spola <95stefano.gagliardi@gmail.com>
2017-10-22 20:49:15 +0200

    add 'loaded' class to body

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/**
+ * Main customizable javascript component of freedom template
+ *
+ * Author: Stefano Gagliardi
+ * License: MIT
+ * Documentation and repository: https://github.com/Spolaa/freedom-template
+ */
+
+/**
+ * Index
+ */
+
+/* Page preloader*/
+$(function(){
+    setTimeout(function(){
+        $('body').addClass('loaded');
+    }, 3000);
+});
